255a2939c5b6c62e2b8e3191f97640b1adeb0fce
rwsvi <rwsvi@DESKTOP-8EJ4AKE>
2021-12-16 16:24:14 -0600

    Code Finished.

--- public_html/SlotMachineCode.js
+++ public_html/SlotMachineCode.js
@@ -1,7 +1,30 @@
-var TRY = true;
+const symbols = ["Cherries", "Oranges", "Plums", "Bells", "Melons", "Bars"];
+const playAgain = true;
 
-var SUCCEED = false;
+document.write ("Your plays are<br>");
 
-while (!(TRY = SUCCEED)) {
-    var SUCCEED = confirm("Have you succeeded yet?");
+RNG();
+
+function RNG() {
+    const chosenSymbols = [];
+    var userBet = Number(prompt("Please place your bet."));
+    for (let i = 0; i <3; i++) {
+        chosenSymbols.push(symbols[Math.floor(Math.random()*symbols.length)]);
+    }
+    document.write ("<br><br>" + chosenSymbols);
+    const chosenSymbols2 = chosenSymbols.filter((e, i, a) => a.indexOf(e) === i);
+    
+    if (chosenSymbols2.length === 3) {
+        var playAgain = confirm("Your symbols are " + chosenSymbols[0] + ", " + chosenSymbols[1] + ", and " + chosenSymbols[2] + ". You lose your bet, " + userBet + "! " + "Would you like to play again?");
+    } else if (chosenSymbols2.length === 2) {
+        userBet = userBet * 2;
+        var playAgain = confirm("Your symbols are " + chosenSymbols[0] + ", " + chosenSymbols[1] + ", and " + chosenSymbols[2] + ". You win double your bet, " + userBet + "! " + "Would you like to play again?");
+    } else if (chosenSymbols2.length === 1) {
+        userBet = userBet * 3;
+        var playAgain = confirm("Your symbols are " + chosenSymbols[0] + ", " + chosenSymbols[1] + ", and " + chosenSymbols[2] + ". You win triple your bet, " + userBet + "! " + "Would you like to play again?");
+    }
+    if (playAgain === true) {
+        RNG();
+    }
 }
+
